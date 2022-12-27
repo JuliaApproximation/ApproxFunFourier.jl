0ca89ecd20f9101a295ee36e41d4779af5227c22
Jishnu Bhattacharya <jishnub.github@gmail.com>
2022-12-27 10:04:43 +0400

    Move some complexroots methods to AFB (#67)

--- src/roots.jl
+++ src/roots.jl
@@ -1,39 +1,3 @@
-
-function companion_matrix(c::Vector{T}) where T
-    n=length(c)-1
-
-    if n==0
-        zeros(T,0,0)
-    else
-        A=zeros(T,n,n)
-        for k=1:n
-            A[k,end]=-c[k]/c[end]
-        end
-        for k=2:n
-            A[k,k-1]=one(T)
-        end
-        A
-    end
-end
-
-
-# if isdir(Pkg.dir("AMVW"))
-#     using AMVW
-#     function complexroots(cfs::Vector)
-#         c=chop(cfs,10eps())
-#
-#         # Only use special routine for large roots
-#         if length(c)≥70
-#             Main.AMVW.rootsAMVW(c)
-#         else
-#             hesseneigvals(companion_matrix(c))
-#         end
-#     end
-# else
-complexroots(cfs::Vector{T}) where {T<:Union{Float64,ComplexF64}} =
-    hesseneigvals(companion_matrix(chop(cfs,10eps())))
-# end
-
 function complexroots(cfs::Vector{T}) where T<:Union{BigFloat,Complex{BigFloat}}
     a = Fun(Taylor(Circle(BigFloat)),cfs)
     ap = a'
@@ -45,8 +9,6 @@ function complexroots(cfs::Vector{T}) where T<:Union{BigFloat,Complex{BigFloat}}
     rts
 end
 
-complexroots(neg::Vector, pos::Vector) =
-    complexroots([reverse(chop(neg,10eps()), dims=1);pos])
 complexroots(f::Fun{Laurent{DD,RR}}) where {DD,RR} =
     mappoint.(Ref(Circle()), Ref(domain(f)),
         complexroots(f.coefficients[2:2:end],f.coefficients[1:2:end]))

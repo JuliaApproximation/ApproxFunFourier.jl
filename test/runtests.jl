92d819199caf871600e69fd1e952dba3d12065a3
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-06-02 14:19:48 +0530

    test with Aqua 0.6 (#94)
    * test with Aqua 0.6
    
    * test formatting on v1.9

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,7 +16,10 @@ using Test
 using Aqua
 @testset "Project quality" begin
     Aqua.test_all(ApproxFunFourier, ambiguities=false,
-        stale_deps=(; ignore=[:ApproxFunBaseTest]))
+        stale_deps=(; ignore=[:ApproxFunBaseTest]), piracy = false,
+        # only test formatting on VERSION >= v1.7
+        # https://github.com/JuliaTesting/Aqua.jl/issues/105#issuecomment-1551405866
+        project_toml_formatting = VERSION >= v"1.9")
 end
 
 @testset "Periodic Domains" begin
